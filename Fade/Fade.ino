da6eab289684deef3234aac224f5431cde14b67e
Brad Lange <blange@gulllakecs.org>
2022-02-28 12:05:26 -0500

    Add PWM to LED6 & LED9 - Fading Stable

--- Fade/Fade.ino
+++ Fade/Fade.ino
@@ -1,4 +1,4 @@
-/*
+ /*
   Fade
 
   This example shows how to fade an LED on pin 9 using the analogWrite()
@@ -16,20 +16,25 @@
 // Programmer: Mr. Lange
 // Date 2.24.2022
 
-int led = 9;           // the PWM pin the LED is attached to
+int ledNine = 9;           // the PWM pin the LED is attached to
+int ledSix = 6;           // the PWM pin the LED is attached to
 int brightness = 0;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
 
 // the setup routine runs once when you press reset:
 void setup() {
   // declare pin 9 to be an output:
-  pinMode(led, OUTPUT);
+  pinMode(ledNine, OUTPUT);
+  // declare pin 6 to be an output:
+  pinMode(ledSix, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   // set the brightness of pin 9:
-  analogWrite(led, brightness);
+  analogWrite(ledNine, brightness);
+  // set the brightness of pin 6:
+  analogWrite(ledSix, brightness);
 
   // change the brightness for next time through the loop:
   brightness = brightness + fadeAmount;
